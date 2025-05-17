2c0ccab489ef07252d00d824f40d2c384f7b1bf6
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2025-05-17 13:41:52 -0400

    stuff

---
+++ web/src/app/api/generate/route.ts
@@ -0,0 +1,49 @@
+import { systemPrompt } from "./systemPrompt";
+//import {userPrompt} from './userPrompt';
+import OpenAI from "openai";
+import { NextRequest, NextResponse } from "next/server";
+
+console.log("hello");
+
+// graveyard for the non working stuf 🪦🪦🪦🪦🪦🪦🪦🪦🪦🪦🪦🪦
+
+// POST is used to send data to a server to create/update a resource
+
+// dummy temp stuff
+
+export async function POST(req: NextRequest) {
+  const openai = new OpenAI({
+    // creates new openai client
+    apiKey: process.env.OPENAI_API_KEY, // accesses the api key from the .env file
+  });
+
+  try {
+    const { prompt, factors, website } = await req.json(); // resume is a string; instagramData is a json object
+
+    // NEED TO BUILD THE USER PROMPT HERE
+    const userPrompt = `prompt: ${prompt} factors: ${JSON.stringify(factors)} website: ${website}`; // this is the user prompt that will be sent to the model
+
+    const local_systemPrompt = systemPrompt; // the system prompt is a set of instructions that tells the model how to behave
+    const local_userPrompt = userPrompt; // the user prompt is the input that the model will respond to
+
+    const completion = await openai.beta.chat.completions.parse({
+      model: "gpt-4o-2024-08-06",
+      messages: [
+        { role: "system", content: local_systemPrompt }, // system --> set behavior
+        { role: "user", content: local_userPrompt }, // user --> the question
+        // assistant --> if u wanted to save chat history i guess (keep it context aware)
+      ],
+      temperature: 0.4,
+    });
+
+    const response = completion.choices[0].message.content; //chatgpt returns an array of choices, so u chose the first one or something idk
+    console.log(response);
+
+    const data = JSON.parse(response);
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error: ", error);
+    return NextResponse.json({ error: "mehek messed up" }, { status: 500 });
+  }
+}
---
+++ web/src/app/api/generate/systemPrompt.ts
@@ -0,0 +1,16 @@
+export const systemPrompt = `You are an AI assitant to a plastic surgeon for breast reconstruction after breast cancer on a website for a plastic surgeon.
+Please give information about breast reconstruction to the potential patient. Be confident in yourself.
+
+Here are some specific tasks you should be able to do:
+
+1. Determine breast implant size based on a formula. 
+If the user asks to determine her breast implant size, ask for these variables, if not already provided: BMI
+
+2. Make a chart comparing the price of different procedures. The output should be in json format. Here is an example:
+{procedure1: price1, procdure2: price2}
+Also, at the beginning of the output concatenate the string "CHART"
+
+3.Answer the user's questions about they might feel with whatever breast implant options (ie. size, material, shape) they should consider.
+This should be based on information from the website.
+
+`
